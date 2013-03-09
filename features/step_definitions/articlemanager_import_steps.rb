caec9e76fd3e17f586e70265394577b7e0248100
David Gullmarsvik <awt2.2012@gmail.com>
2013-03-10 00:02:47 +0100

    removed unused step definition

--- features/step_definitions/articlemanager_import_steps.rb
+++ features/step_definitions/articlemanager_import_steps.rb
@@ -2,11 +2,6 @@ Given(/^an empty ArticleStore$/) do
   @controller = ArticleManager::Controller.new(output)
 end
 
-Given(/^a article record with (\d+) valid article titled "(.*?)"$/) do | count, title |
-  @article_record = empty_article_record
-  @article_record += "2013-01-01, #{title}, http://example.org/2/,Tutorial,Very Descriptive Description\n"
-end
-
 Given(/^an article record with (\d+) "(.*?)"$/) do | count, article |
 	@article_record = empty_article_record
 	@article_record += article
