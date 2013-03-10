4e06b81317fb84384d74a6d6c39d44d5e97a9c25
David Gullmarsvik <awt2.2012@gmail.com>
2013-03-10 02:05:45 +0100

    Unit Test for not adding exception articles with add_array in ArticleRepository

--- spec/articlemanager/article_repository_spec.rb
+++ spec/articlemanager/article_repository_spec.rb
@@ -2,13 +2,21 @@ require 'spec_helper'
 
 module ArticleManager
 	describe ArticleRepository do
+		let (:article_repository) {ArticleRepository.new}
+
 		describe "#add_array" do
 			it "adds all Articles in the array to the repository" do
-				@article_repository = ArticleRepository.new
 				article_array = [Article.new(["2012-01-01", "Title 1", "http://example.org/1/", "", ""]), Article.new(["2012-01-02", "Title 2", "http://example.org/2/", "", ""])]
-				@article_repository.add_array(article_array)
-				stored_articles = @article_repository.find_all
+				article_repository.add_array(article_array)
+				stored_articles = article_repository.find_all
 				stored_articles.length.should == 2
+			end
+
+			it "does not add ExceptionArticles to the repository" do
+				article_array = [Article.new(["2012-01-01", "Title 1", "http://example.org/1/", "", ""]), ExceptionArticle.new("Title Missing")]
+				article_repository.add_array(article_array)
+				stored_articles = article_repository.find_all
+				stored_articles.length.should == 1
 			end
 
 		end
