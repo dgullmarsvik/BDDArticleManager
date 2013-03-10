d88291a31873a966c288060ee394600c1998184a
David Gullmarsvik <awt2.2012@gmail.com>
2013-03-10 02:01:02 +0100

    Implementation for add_array in ArticleRepository

--- lib/articlemanager/article_repository.rb
+++ lib/articlemanager/article_repository.rb
@@ -1,4 +1,19 @@
 module ArticleManager
 	class ArticleRepository
+		def initialize
+			@internal_storage = []
+		end
+
+		def add_array(article_array)
+			article_array.each { | article | add(article) }
+		end
+
+		def add(article)
+			@internal_storage << article
+		end
+
+		def find_all
+			return @internal_storage
+		end
 	end
 end
--- spec/articlemanager/article_repository_spec.rb
+++ spec/articlemanager/article_repository_spec.rb
@@ -8,6 +8,7 @@ module ArticleManager
 				article_array = [Article.new(["2012-01-01", "Title 1", "http://example.org/1/", "", ""]), Article.new(["2012-01-02", "Title 2", "http://example.org/2/", "", ""])]
 				@article_repository.add_array(article_array)
 				stored_articles = @article_repository.find_all
+				stored_articles.length.should == 2
 			end
 
 		end
